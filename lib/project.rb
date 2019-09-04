2baaea5e932155540517c68f2b2fd3f74e3d5887
yaoyu bao <yaobao5354@gmail.com>
2019-09-04 21:27:39 +0000

    Automatically backed up by Learn

---
+++ lib/project.rb
@@ -0,0 +1,15 @@
+class Project
+  attr_accessor :backers
+  attr_reader :title 
+  
+  def initialize(title)
+    @title = title
+    @backers = []
+  end
+  
+  def add_backer(backer)
+    @backers << backer
+    backer.back_project(self)
+  end 
+  
+end 
