ee8d29ef88c6d7367d4453b57764d25b7ebad308
yaoyu bao <yaobao5354@gmail.com>
2019-09-04 21:39:52 +0000

    Done.

---
+++ lib/project.rb
@@ -0,0 +1,15 @@
+class Project
+  attr_accessor :backers
+  attr_reader :title 
+  
+  def initialize(title)
+    @title = title
+    @backers = []
+  end
+  
+  def add_backer(backer)
+    self.backers << backer
+    backer.backed_projects << self 
+  end 
+  
+end 
